035f29e8ee2f8e60123854ed41e4dbef3c036d51
korabieka <hussienashour@ymail.com>
2016-03-31 15:00:36 +0200

    lol

---
+++ Aqarmap/subscribtions/urls.py
@@ -0,0 +1,9 @@
+from django.conf.urls import url
+from . import views
+
+urlpatterns = [
+	url(r'^$', views.subscribtions, name='subscribtions'),
+	#ex addProperty/5 - send <prop_id> as an arguments | [0-9]+ sequences of digits
+	#url(r'^(?P<prop_id>[0-9]+)/$', views.details, name='prop_details'),
+	#url(r'^(?P<addProperty_id>[0-9])/$', views.addProperty, name='add'),
+]
--- Aqarmap/subscribtions/views.py
+++ Aqarmap/subscribtions/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
+from django.http import HttpRequest,HttpResponse,Http404
+from .models import Subscribtions
+def subscribtions(request,user_id):
+	pass
 
-# Create your views here.
